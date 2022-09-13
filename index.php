2beaaa13f34ae1e7cd2b84539c35f8832f7217a4
PaiGit759 <107637697+PaiGit759@users.noreply.github.com>
2022-09-13 11:34:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> PAI </title>
+    <meta charset='utf-8'>
+    <script>
+    </script>
+
+</head>
+
+<body>
+    <p>Form to send ********</p>
+
+<form action="form.php" method="post" enctype="multipart/form-data">
+
+    <input type="file" name="filename"><br>
+    <input type="submit" value="Send">
+
+</form>
+
+<?php
+//phpinfo();
+
+?>  
+
+</body>
+
+</html>
